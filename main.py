144e85b4a72e6b9c237a2fbd28d56568a995c9d7
Natalia Marin <155592997+N4tth@users.noreply.github.com>
2024-11-12 08:57:02 -0500

    Interfaz

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from interface import FileConverterApp
+
+def main():
+
+		root = tk.Tk()
+		app = FileConverterApp(root)
+		root.mainloop()
+
+if __name__ == "__main__":
+		main()
